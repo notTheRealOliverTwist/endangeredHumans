2069d67a262d779a01c870d6a5a7d3e3810d9ee8
notTheRealOliverTwist <notreallyolivertwist@gmail.com>
2022-03-29 15:07:47 +0200

    initial commit

---
+++ js/browserifyConversions.js
@@ -0,0 +1,4 @@
+var country_data = require('../data/countries+states+cities.json');
+
+global.window.country_info = country_data;
+
--- js/index.js
+++ js/index.js
@@ -1,7 +1,9 @@
 function $(id){ return document.getElementById(id); }
 
 var $j = jQuery.noConflict();
-/*
+
+var countries = window.country_info;
+
 
 // Require the google trends api
 //const googleTrends = require('google-trends-api');
@@ -11,38 +13,105 @@ var $j = jQuery.noConflict();
 const runAnalysis = $('divaa1');
 const trendOptions = $('trend-options');
 
-var primeRateCountries = [];
+var Countries = [];
+var States = [];
+var Cities = [];
 
-/*$j(document).ready(function() {
-	var countryMenu = $("countries");
-	var i = 0;
-	console.log('This should be running');
-	fetch("https://restcountries.com/v2/all").then(res => {
-		return res.json();
-	}).then(countries => {
-		console.log(countries);
-		let output = "";
-		countries.forEach(country =>{
-			output +=
-				`<option id="${country.name}" value="${country.alpha2Code}">${country.name}</option>`;
-			primeRateCountries[i] = country.name;
-			i++;
-			console.log(output);
-		})
-		countryMenu.innerHTML = output;
-	}).catch(error => {
-		console.log(error);
-	});
+var countryMenu = $("countryMenu");
+var stateMenu = $("stateMenu");
+var cityMenu = $("cityMenu");
+
+$j(document).ready(function() {
+
+	let output = "";
+	for(var i = 0 ; i < countries.length; ++i){
+		output +=
+			`<option id="${countries[i].iso2}" value="${countries[i].name}">${countries[i].name}</option>`;
+		Countries[i] = countries[i].name;
+		console.log(output);
+	}
+	countryMenu.innerHTML = output;
+
 });
 
+function chooseState(){
+	var countryChosen = countryMenu.value;
 
+	var stateMenu = $('stateMenu');
+	stateMenu.innerHTML = '';
+	var cityMenu = $('cityMenu');
+
+	console.log('state running');
+	console.log(countryChosen);
+	console.log('----------------------');
+	console.log(Countries);
+
+	let stateOutput = "";
+
+	for(var i = 0 ; i < countries.length; ++i){
+		console.log('forloop running');
+		if(i === Countries.indexOf(countryChosen)){
+			for(var j = 0; j < countries[i].states.length; ++j){
+				stateOutput +=
+					`<option id="${countries[i].states[j].name}" value="${countries[i].states[j].name}">${countries[i].states[j].name}</option>`;
+				States[j] = countries[i].states[j].name;
+				console.log(stateOutput);
+			}
+		}
+		stateMenu.innerHTML = stateOutput;
+	}
+}
+
+function chooseCity(){
+	var stateMenu = $('stateMenu');
+	var cityMenu = $('cityMenu');
+
+	cityMenu.innerHTML = '';
+
+	var countryChosen = countryMenu.value;
+	var stateChosen = stateMenu.value;
+
+	var stateMenu = $('stateMenu');
+	var cityMenu = $('cityMenu');
+
+	console.log('state running');
+	console.log(stateChosen);
+	console.log('----------------------');
+	console.log(States);
+
+	let cityOutput = "";
+
+	for(var i = 0 ; i < countries.length; ++i){
+		console.log('forloop running');
+		if(i === Countries.indexOf(countryChosen)){
+			for(var j = 0; j < countries[i].states.length; ++j){
+				if(j === States.indexOf(stateChosen)){
+					for(var k = 0; k < countries[i].states[j].cities.length; ++k){
+						cityOutput +=
+							`<option id="${countries[i].states[j].cities[k].name}" value="${countries[i].states[j].cities[k].name}">${countries[i].states[j].cities[k].name}</option>`;
+						Cities[j] = countries[i].states[j].cities[k].name;
+						console.log(cityOutput);
+					}
+				}
+			}
+			//              cityMenu.innerHTML = cityOutput;
+		}
+		cityMenu.innerHTML = cityOutput;
+	}
+}
+
+////////////////
 const daily = $('btna1');
 const realTime = $('btna2');
 const overTime = $('btna3');
 
-const countriesList = $('countries');
-const calendar = $('calendar');
-const keywords = $('keyword');
+const countriesList = $('country-menu');
+const calendar = $('calendar-menu');
+const keywords = $('keyword-menu');
+const optionsBar = $('trend-list');
+
+const stateList = $('state-menu');
+const cityList = $('city-menu');
 
 const usingTrend = ['Daily', 'RealTime', 'OverTime'];
 
@@ -50,42 +119,43 @@ var trend = '';
 
 ////////////////////////////////////////////////////////////////
 
-/* Finish editing the below 
+/* Finish editing the below */
 
 daily.addEventListener('click', function(){
 	countriesList.classList.remove('hideOptions');
 	calendar.classList.remove('hideOptions');
+	optionsBar.classList.remove('hideOptions');
 
 	console.log('Daily is running');
 
 	(!keyword.classList.contains('hideOptions'))?
-		keyword.classList.add('hideOptions'): null;
-
+		keyword.classList.add('hideOptions'): null;	
 	trend = usingTrend[0];
 });
 
 realTime.addEventListener('click', function(){
-        countriesList.classList.remove('hideOptions');
+	countriesList.classList.remove('hideOptions');
+	optionsBar.classList.remove('hideOptions');
 
-        (!calendar.classList.contains('hideOptions'))?
-                calendar.classList.add('hideOptions'): null;
+	(!calendar.classList.contains('hideOptions'))?
+		calendar.classList.add('hideOptions'): null;
 
-        (!keywords.classList.contains('hideOptions'))?
-                keywords.classList.add('hideOptions'): null;
+	(!keywords.classList.contains('hideOptions'))?
+		keywords.classList.add('hideOptions'): null;
 	trend = usingTrend[1];
 });
 
 overTime.addEventListener('click', function(){
-        keyword.classList.remove('hideOptions');
+	keyword.classList.remove('hideOptions');
+	optionsBar.classList.remove('hideOptions');
 
-        (!calendar.classList.contains('hideOptions'))?
-                calendar.classList.add('hideOptions'): null;
 
-        (!countriesList.classList.contains('hideOptions'))? 
-                countriesList.classList.add('hideOptions'): null;
+	(!calendar.classList.contains('hideOptions'))?
+		calendar.classList.add('hideOptions'): null;
+
+	(!countriesList.classList.contains('hideOptions'))? 
+		countriesList.classList.add('hideOptions'): null;
 	trend = usingTrend[2];
-
-
 });
 
 
@@ -95,9 +165,9 @@ overTime.addEventListener('click', function(){
 function googleDailyTrends()
 {
 	// This should be the element id of a drop down or something like that
-	var geoLocation = $('countries').toString();
-	var chosenDate = $('calendar').toString();
-	var chosenKeyword = $('keyword').toString();
+	var geoLocation = $('countries').value;
+	var chosenDate = $('calendar').value;
+	var chosenKeyword = $('keyword').value;
 
 	switch(trend){
 		case('Daily'):
@@ -139,5 +209,4 @@ function googleDailyTrends()
 	}
 }
 
-runAnalysis.addEventListener('onlick', googleDailyTrends);
-*/
+//runAnalysis.addEventListener('onlick', googleDailyTrends);
